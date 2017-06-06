69fbbfc68487d18e7b1e9aa94aa25306cab2fedd
ojs <contact@ollyjsmith.com>
2017-06-06 17:19:51 +0200

    auto add specific class to links around img tags (in posts) to facilitate styling

--- inc/dc_template-tags.php
+++ inc/dc_template-tags.php
@@ -5,6 +5,29 @@
 		return $images[0];
 	}
 
+	function dc_add_class_to_linked_images( $html ){
+		// with thanks to: https://stackoverflow.com/a/30540598
+    $classes = 'image-link'; // can do multiple classes, separate with space
+
+    $patterns = array();
+    $replacements = array();
+
+    $patterns[0] = '/<a(?![^>]*class)([^>]*)>\s*<img([^>]*)>\s*<\/a>/'; // matches img tag wrapped in anchor tag where anchor tag where anchor has no existing classes
+    $replacements[0] = '<a\1 class="' . $classes . '"><img\2></a>';
+
+    $patterns[1] = '/<a([^>]*)class="([^"]*)"([^>]*)>\s*<img([^>]*)>\s*<\/a>/'; // matches img tag wrapped in anchor tag where anchor has existing classes contained in double quotes
+    $replacements[1] = '<a\1class="' . $classes . ' \2"\3><img\4></a>';
+
+    $patterns[2] = '/<a([^>]*)class=\'([^\']*)\'([^>]*)>\s*<img([^>]*)>\s*<\/a>/'; // matches img tag wrapped in anchor tag where anchor has existing classes contained in single quotes
+    $replacements[2] = '<a\1class="' . $classes . ' \2"\3><img\4></a>';
+
+    $html = preg_replace($patterns, $replacements, $html);
+
+    return $html;
+	}
+
+	add_filter('the_content', 'dc_add_class_to_linked_images', 100, 1);
+
 	function dc_get_notes_url(){
 		return get_permalink( get_option( 'page_for_posts' ) );
 	}
