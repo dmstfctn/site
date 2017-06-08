2a326e813ad29d549ec97c1b3b9bf6bcc6ca6c16
ojs <contact@ollyjsmith.com>
2017-06-08 12:26:54 +0200

    add click to trigger

--- assets/javascript/src/modules/HoverImg.js
+++ assets/javascript/src/modules/HoverImg.js
@@ -5,6 +5,7 @@ var HoverImg = function( _ele ){
   this.ele = this.$ele.get(0);
 	this.$trigger = this.$ele.parent().find('.dc-hoverimg-trigger');
 	this.replaced = false;
+	this.active = false;
 	this.addListeners();
 }
 
@@ -13,15 +14,35 @@ var proto = HoverImg.prototype;
 proto.addListeners = function(){
 	var that = this;
 	this.$trigger.on( 'mouseenter', function(){
-		console.log( 'activate hoverimg on', that.$ele );
-		that.$ele.addClass('active');
-		that.$ele.parent().addClass('hoverimg-active');
-		that.setSrc();
+		that.activate();
 	});
 	this.$trigger.on( 'mouseleave', function(){
-		that.$ele.parent().removeClass('hoverimg-active');
-		that.$ele.removeClass('active');
+		that.deactivate();
 	});
+	this.$trigger.on( 'click', function(){
+		that.toggle();
+	});
+}
+
+proto.toggle = function(){
+	if( this.active ){
+		this.deactivate();
+	} else {
+		this.activate();
+	}
+}
+
+proto.activate = function(){
+	this.$ele.addClass('active');
+	this.$ele.parent().addClass('hoverimg-active');
+	this.setSrc();
+	this.active = true;
+}
+
+proto.deactivate = function(){
+	this.$ele.parent().removeClass('hoverimg-active');
+	this.$ele.removeClass('active');
+	this.active = false;
 }
 
 proto.setSrc = function(){
