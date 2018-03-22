d631810ef66efa05a73a385fb3ebaa7dfc687aa2
ojs <contact@ollyjsmith.com>
2018-03-22 15:08:47 +0000

    move projects to 2 column + title format

--- assets/javascript/src/modules/Project.js
+++ assets/javascript/src/modules/Project.js
@@ -3,6 +3,7 @@ var Quadrant = require( './Quadrant.js' );
 
 var Project = function( _ele ){
 	Quadrant.call( this, _ele );
+	this.renderSections = $.extend({}, this.sections);
 }
 
 Project.prototype = Object.create( Quadrant.prototype );
@@ -11,33 +12,46 @@ Project.prototype.constructor = Project;
 var proto = Project.prototype;
 
 proto.getSections = function(){
-	this.$title = this.$ele.find('.project-section__title');
 	this.$images = this.$ele.find('.project-section__images');
 	this.$description = this.$ele.find('.project-section__description');
-	this.$close = this.$ele.find('.project-section__close');
 }
 
 proto.render = function(){
 	this.$images
-		.css( this.sections[0].css )
-		.attr('data-section-location', this.sections[0].name )
-		.attr('data-proportion', this.calculateQuadrantProportion( this.sections[0].css ) );
+		.css( this.renderSections[0].css )
+		.attr('data-section-location', this.renderSections[0].name )
+		.attr( 'data-section-span', this.largestColumn )
+		.attr('data-proportion', this.calculateQuadrantProportion( this.renderSections[0].css ) );
 
 	this.$description
-		.css( this.sections[1].css )
-		.attr('data-section-location', this.sections[1].name )
-		.attr('data-proportion', this.calculateQuadrantProportion( this.sections[1].css ) );
+		.css( this.renderSections[1].css )
+		.attr('data-section-location', this.renderSections[1].name )
+		.attr( 'data-section-span', this.smallestColumn )
+		.attr('data-proportion', this.calculateQuadrantProportion( this.renderSections[1].css ) );
 
-	this.$title
-		.css( this.sections[2].css )
-		.attr('data-section-location', this.sections[2].name )
-		.attr('data-proportion', this.calculateQuadrantProportion( this.sections[2].css ) );
-
-	this.$close
-		.css( this.sections[3].css )
-		.attr('data-section-location', this.sections[3].name )
-		.attr('data-proportion', this.calculateQuadrantProportion( this.sections[3].css ) )
 }
+
+proto.calculateHierarchy = function(){
+	this.largestColumn = 'w';
+	this.smallestColumn = 'e';
+	// sorts each section with [0] being the largest, [3] smallest
+	this.sections.sort(function( a, b ){
+		return (a.css.width*a.css.height) < (b.css.width*b.css.height);
+	});
+	if( this.sections[0].name === 'ne' || this.sections[0].name === 'se' ){
+		this.largestColumn = 'e';
+		this.smallestColumn = 'w';
+	}
+	this.renderSections = [
+		this.sections[0] //largest column
+	];
+	for( var i = 1; i < this.sections.length; i++ ){
+		if( this.sections[i].name.indexOf( this.smallestColumn ) !== -1 ){
+			this.renderSections.push( this.sections[i] );
+			break;
+		}
+	}
+};
 
 proto.destroy = function(){
 	this.$images
--- single-dc_project.php
+++ single-dc_project.php
@@ -2,24 +2,24 @@
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <div class="layer layer__project project">
-	<article class="quadrant-wrapper project-wrapper quadrant-wrapper__<?php echo dc_get_post_theme( get_the_ID() )->slug; ?>" id="project">
-		<header class="quadrant-section project-section project-section__title" data-section-location="nw">
-			<h1>
-				<?php the_title(); ?>
-			</h1>
-		</header><!--
+	<article class="quadrant-wrapper project-wrapper quadrant-wrapper__<?php echo dc_get_post_theme( get_the_ID() )->slug; ?>" id="project"><!--
 	--><section class="quadrant-section project-section project-section__images dc-slideshow">
 			<?php dc_project_media(); ?>
 		</section><!--
 	--><section class="quadrant-section project-section project-section__description">
+			<header>
+				<h1>
+					<?php the_title(); ?>
+				</h1>
+			</header>
 			<div class="wysiwyg">
 				<?php the_content(); ?>
 			</div>
 		</section><!--
-	--><section class="quadrant-section project-section project-section__close" data-section-location="se">
+	--><section class="project-section project-section__close">
 			<a href="<?php echo home_url() ?>" class="text-link no-decoration-link quadrant-close-link">Close</a>
-		</section>
-	</article>
+		</section><!--
+--></article>
 </div>
 
 <?php endwhile; endif; ?>
