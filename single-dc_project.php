172933dbe1e1d1b208f66b85d837bf4b0802197a
ojs <contact@ollyjsmith.com>
2018-04-27 17:54:12 +0100

    multiple fixes and tweaks to tabs, mob version, related to font change, etc

--- single-dc_project.php
+++ single-dc_project.php
@@ -1,7 +1,25 @@
 <?php get_header(); ?>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<div class="layer layer__project project">
+<div class="layer layer__project project layer__<?php echo dc_get_post_theme( get_the_ID() )->slug; ?>">
+	<div class="about-fake-tabs">
+		<div class="theme">
+			<div class="theme--leader">
+				<h1>
+					<a href="<?php echo home_url('#offshore'); ?>">
+						Offshore
+					</a>
+				</h1>
+			</div>
+		</div>
+		<div class="theme theme__networks">
+			<div class="theme--leader">
+				<a href="<?php echo home_url('#networks'); ?>">
+					<h1>Networks</h1>
+				</a>
+			</div>
+		</div>
+	</div>
 	<article class="quadrant-wrapper project-wrapper quadrant-wrapper__<?php echo dc_get_post_theme( get_the_ID() )->slug; ?>" id="project"><!--
 	--><section class="quadrant-section project-section project-section__images dc-slideshow">
 			<?php dc_project_media(); ?>
--- single.php
+++ single.php
@@ -2,6 +2,24 @@
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<div class="layer layer__post post">
+		<div class="about-fake-tabs">
+			<div class="theme">
+				<div class="theme--leader">
+					<h1>
+						<a href="<?php echo home_url('#offshore'); ?>">
+							Offshore
+						</a>
+					</h1>
+				</div>
+			</div>
+			<div class="theme theme__networks">
+				<div class="theme--leader">
+					<a href="<?php echo home_url('#networks'); ?>">
+						<h1>Networks</h1>
+					</a>
+				</div>
+			</div>
+		</div>
 		<?php include 'post.php'; ?>
 	</div>
 <?php endwhile; endif; ?>
