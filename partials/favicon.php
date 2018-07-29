d38859ef1f6b15ede966063962567e9bec99d0ea
ojs <contact@ollyjsmith.com>
2018-07-29 14:44:15 +0100

    simplify favicon shit

---
+++ partials/favicon.php
@@ -0,0 +1,5 @@
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/favicon/a180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="<?php echo get_stylesheet_directory_uri(); ?>/assets/favicon/d192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/favicon/f32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/favicon/f96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/favicon/f16.png">
